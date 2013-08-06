850d0c04b40d73e1e76ae9c3e3f87d5753fb3bd2
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-08-06 15:44:04 +0530

    added few features to TaskList  * ignoreErrors option to not to stop at errors  * at the end of the all tests, we get a object with summeries for each session

--- tests/taskList.js
+++ tests/taskList.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 suite('TaskList', function() {
   test('register and run', function(done) {
     var optionsList = [];
-    var session = {};
+    var session = {_host: 'host'};
     TaskList.registerTask('simpleTask', function(_session, options, callback) {
       assert.equal(session, _session);
       optionsList.push(options);
@@ -14,9 +14,60 @@ suite('TaskList', function() {
     var taskList = new TaskList('simple', {pretty: false});
     taskList.simpleTask('Simple Name', {aa: 10});
     taskList.simpleTask('Simple Name2', {aa: 20});
-    taskList.run(session, function(errors) {
-      assert.deepEqual(errors, []);
+    taskList.run(session, function(summeryMap) {
+      assert.ok(summeryMap[session._host]);
       assert.deepEqual(optionsList, [{aa: 10}, {aa: 20}]);
+      done();
+    });
+  });
+
+  test('when error', function(done) {
+    var session = {_host: 'host'};
+    TaskList.registerTask('simpleTask2', function(_session, options, callback) {
+      assert.equal(session, _session);
+      if(options.aa == 20) {
+        callback(new Error('error-here'));
+      } else {
+        callback();
+      }
+    });
+
+    var taskList = new TaskList('simple', {pretty: false});
+    taskList.simpleTask2('one', {aa: 10});
+    taskList.simpleTask2('two', {aa: 20});
+    taskList.simpleTask2('three', {aa: 30});
+    taskList.run(session, function(summeryMap) {
+      assert.ok(summeryMap[session._host]);
+      assert.deepEqual(summeryMap[session._host], [
+        {action: 'one', status: 'SUCCESS'},
+        {action: 'two', status: 'FAILED', error: 'error-here'}
+      ]);
+      done();
+    });
+  });
+
+  test('when error - with ignoreErrors', function(done) {
+    var session = {_host: 'host'};
+    TaskList.registerTask('simpleTask3', function(_session, options, callback) {
+      assert.equal(session, _session);
+      if(options.aa == 20) {
+        callback(new Error('error-here'));
+      } else {
+        callback();
+      }
+    });
+
+    var taskList = new TaskList('simple', {pretty: false, ignoreErrors: true});
+    taskList.simpleTask3('one', {aa: 10});
+    taskList.simpleTask3('two', {aa: 20});
+    taskList.simpleTask3('three', {aa: 30});
+    taskList.run(session, function(summeryMap) {
+      assert.ok(summeryMap[session._host]);
+      assert.deepEqual(summeryMap[session._host], [
+        {action: 'one', status: 'SUCCESS'},
+        {action: 'two', status: 'FAILED', error: 'error-here'},
+        {action: 'three', status: 'SUCCESS'},
+      ]);
       done();
     });
   });
